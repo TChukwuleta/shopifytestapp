eb90b14459fb18e684c842f8aa56d73a7f482b84
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-03-18 19:24:44 +0100

    test app

--- extensions/tonse/src/Checkout.jsx
+++ extensions/tonse/src/Checkout.jsx
@@ -20,15 +20,13 @@ function Extension() {
   const { shop, checkoutToken } = useApi();
   const [isLoading, setIsLoading] = useState(true);
   const [isSuccess, setIsSuccess] = useState(false);
+  const baseUrl = "https://6d87-102-89-23-165.ngrok-free.app/stores/JC52vAtXMKK2C8wyoXDPuGB1wAcUoHqFnzYdyNhEoEZV/stores/JC52vAtXMKK2C8wyoXDPuGB1wAcUoHqFnzYdyNhEoEZV/plugins/shopify-v2";
   const hasManualPayment = options.some((option) => option.type.toLowerCase() === 'manualpayment');
-  const createInvoiceUrl = `https://6d87-102-89-23-165.ngrok-free.app/stores/JC52vAtXMKK2C8wyoXDPuGB1wAcUoHqFnzYdyNhEoEZV/plugins/shopify-v2/create-invoice?checkout_token=${checkoutToken.current}`;
-  const checkoutUrl = `https://6d87-102-89-23-165.ngrok-free.app/stores/JC52vAtXMKK2C8wyoXDPuGB1wAcUoHqFnzYdyNhEoEZV/plugins/shopify-v2/checkout?checkout_token=${checkoutToken.current}`;
-
-  console.log("Selected Payment Options:", options);
+  const createInvoiceUrl = `${baseUrl}/create-invoice?checkout_token=${checkoutToken.current}`;
+  const checkoutUrl = `${baseUrl}/checkout?checkout_token=${checkoutToken.current}`;
 
   useEffect(() => {
     if (!hasManualPayment) return;
-
     const fetchInvoice = async () => {
       setIsLoading(true);
       try {
@@ -39,9 +37,8 @@ function Extension() {
         if (response.ok) {
           setIsSuccess(true);
         }
-      } catch (error) {
-        console.error("Failed to create invoice:", error);
-      } finally {
+      } catch (error) {} 
+      finally {
         setIsLoading(false);
       }
     };
@@ -49,34 +46,19 @@ function Extension() {
     return () => clearTimeout(timer)
   }, [hasManualPayment]);
 
-  if (!hasManualPayment || (!isLoading && !isSuccess)) return null;
+  if (!hasManualPayment) return null;
 
   return (
     <BlockStack>
-      {isLoading ? (
-        <Spinner />
-      ) : isSuccess ? (
+      {isLoading && <Spinner />}
+      {!isLoading && isSuccess && (
         <>
           <Text>Shop name: {shop.name}</Text>
-          <Text size="large" emphasis="bold">
-            Selected Payment Options:
-          </Text>
-          {options.length > 0 ? (
-            options.map((option, index) => (
-              <BlockStack key={`selected-${index}`} border="base" padding="tight">
-                {Object.entries(option).map(([key, value]) => (
-                  <Text key={key}>
-                    <Text emphasis="bold">{key}:</Text> {JSON.stringify(value)}
-                  </Text>
-                ))}
-              </BlockStack>
-            ))
-          ) : (
-            <Text>No payment options selected.</Text>
-          )}
-          <Button to={checkoutUrl} external>Complete Test Payment</Button>
+          <Text size="large" alignment="center" bold>Review and pay using BTCPay Server!</Text>
+          <Text>Please review your order and complete the payment using BTCPay Server.</Text>
+          <Button to={checkoutUrl} external>Complete Payment</Button>
         </>
-      ) : null}
+      )}
     </BlockStack>
   );
   
